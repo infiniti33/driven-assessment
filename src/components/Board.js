518b3f4bc3650eee79787b9ddf71509818def0da
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-04-14 15:28:21 -0700

    update Board component to use pieceState

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,31 +3,22 @@ import React from 'react';
 import Row from './Row';
 
 const Board = props => {
-  const { nRows } = props;
+  const { nRows, pieceState } = props;
+
+  if (!pieceState) return <p>Loading...</p>
 
   const renderRows = numRows => {
     const rows = [];
     let isBlack = true;
-    const lastTwoRows = numRows - 2;
 
     for (let i = 0; i < numRows; i++) {
-      let hasInitialPiece = false;
-      let initialPieceColor = 'red';
-
-      if (i < 2) hasInitialPiece = true;
-      if (i >= lastTwoRows) {
-        hasInitialPiece = true;
-        initialPieceColor = 'black';
-      }
-
       rows.push(
         <Row
           columns={nRows}
           startBlack={isBlack}
           key={`row${i}`}
           row={i}
-          hasInitialPiece={hasInitialPiece}
-          initialPieceColor={initialPieceColor}
+          rowState={pieceState[i]}
         />
       );
       isBlack = !isBlack;
