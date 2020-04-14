155a68fe3d6dc1f93de56cfa0e668427ce5087f2
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-04-14 14:01:21 -0700

    add gamePiece logic to square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../styles/styles.css'
+import GamePiece from './GamePiece';
 
 const Square = props => {
-  const { color } = props;
+  const { color, hasInitialPiece, initialPieceColor, row, col } = props;
+
+  let pieceColor = initialPieceColor;
+  let hasGamePiece = hasInitialPiece;
 
   return (
     <div className={`square ${color}`}>
+      {hasGamePiece && <GamePiece row={row} col={col} color={pieceColor} />}
     </div>
-  )
-}
+  );
+};
 
 export default Square;
