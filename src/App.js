be10c4348df9f49387b556bf2462226ae954c96d
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-04-14 15:27:49 -0700

    add handleColorChange, initiateBoard to App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Board from './components/Board';
-import './styles/styles.css';
+import ColorRadio from './components/ColorRadio';
 
 function App() {
   const [nRows, setNRows] = useState(8);
   const [inputText, setInputText] = useState('');
+  const [pieceState, setPieceState] = useState(null);
+  const [pOneColor, setPOneColor] = useState('red');
+  const [pTwoColor, setPTwoColor] = useState('black');
 
-  const handleInputChange = value => {
+  useEffect(() => {
+    const initiateBoard = rows => {
+      const lastTwo = rows - 2;
+      const rowsArr = new Array(rows).fill('');
+      const rowsAndColsArr = rowsArr.map(() => new Array(rows).fill(''));
+      const initialBoard = rowsAndColsArr.map((row, idx) => {
+        return row.map(() => {
+          const player = idx < 2 ? 1 : idx >= lastTwo ? 2 : '';
+          const color = idx < 2 ? pOneColor : idx >= lastTwo ? pTwoColor : '';
+          return {
+            color,
+            player,
+          };
+        });
+      });
+      setPieceState(initialBoard);
+    };
+    initiateBoard(nRows);
+  }, []);
+
+  const handleBoardSizeChange = value => {
     setInputText(value);
     if (Number(value) > 0) {
-      setNRows(Number(value))
+      setNRows(Number(value));
     }
-  }
+  };
+
+  const handleColorChange = (color, player) => {
+    setPieceState(pieceState.map(row => {
+      return row.map(square => {
+        if (square.player === player) {
+          square.color = color;
+        };
+        return square;
+      });
+    }));
+  };
 
   return (
     <div className='App'>
-      <Board className='board' nRows={nRows} />
+      <Board className='board' nRows={nRows} pieceState={pieceState} />
       <div className='controls-container'>
-        <input type='text' value={inputText} onChange={(e) => handleInputChange(e.target.value)} />
+        <p className='controls-text'>
+          Board Size
+        </p>
+        <input type='text' value={inputText} onChange={(e) => handleBoardSizeChange(e.target.value)} />
+        <ColorRadio player={1} handleChange={handleColorChange} />
+        <ColorRadio player={2} handleChange={handleColorChange} />
       </div>
-    </div>
+    </div >
   )
 }
 
