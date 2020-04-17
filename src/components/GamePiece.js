a8c3a54ed0c87a0b9e87aad27098dee402c12aa8
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-04-17 12:50:41 -0700

    added styles object to gamePiece to handle shape/color

--- src/components/GamePiece.js
+++ src/components/GamePiece.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 const GamePiece = props => {
-  const { color } = props;
+  const { color, shape } = props;
+
+  const makeStyle = (color, shape) => {
+    const style = {}
+    if (shape === 'square' || shape === 'circle') {
+      style.height = '25px';
+      style.width = '25px';
+      style.backgroundColor = color;
+    }
+    if (shape === 'circle') {
+      style.borderRadius = '50%';
+    }
+    if (shape === 'triangle') {
+      style.width = 0;
+      style.height = 0;
+      style.borderLeft = '12px solid transparent';
+      style.borderRight = '12px solid transparent';
+      style.borderBottom = `24px solid ${color}`;
+    }
+    return style;
+  };
 
   return (
-    <div className={`game-piece`} style={{ 'backgroundColor': color }} />
+    <div className={`game-piece`} style={makeStyle(color, shape)} />
   );
 };
 
